04abc4c6bf24170c746cf0b56e98732339e2105c
thomas messett <thomas.messett@stampapp.me>
2022-06-20 23:01:24 +0100

    added minute basics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,32 +18,40 @@ class PagesController < ApplicationController
     if params[:hours] != nil || params[:mins] != nil || params[:tracks] != nil
       selected_playlists = params[:selected_playlists]
       number_tracks = params[:tracks]
-      track_length_mins = (params[:hours] * 60) + params[:mins]
+      track_length_seconds = (params[:hours].to_i * 3600) + (params[:mins].to_i * 60)
+      all_tracks = build_playlist_number(selected_playlists)
       if number_tracks.to_i > 0
-        all_tracks = build_playlist_number(selected_playlists, number_tracks)
+        playlist = all_tracks.sample(number_tracks.to_i)
+      elsif track_length_seconds > 0
+        random = (0..(all_tracks.length - 1)).to_a.shuffle
         playlist = []
+        length = []
         loop do
-          n = rand(all_tracks.length)
+          n = random.pop
           playlist << all_tracks[n]
-          if playlist.length >= number_tracks.to_i
+          duration = all_tracks[n].duration_ms
+          length << (duration / 1000)
+          if length.sum >= track_length_seconds || random.length == 0
             break
           end
         end
+        raise
       end
       playlist.each do |song|
-        add = play(song[:id])
-        @list_added << [song[:name], add]
+        add = play(song.id)
+        raise
+        @list_added << [song.name, add]
       end
     end
   end
 
-  def build_playlist_number(selected_playlists, number_tracks)
+  def build_playlist_number(selected_playlists)
     RSpotify.authenticate(ENV["SPOTIFY_KEY_ID"], ENV["SECRET_KEY"])
     tracks = []
     selected_playlists.each do |playlist|
       list = RSpotify::Playlist.find_by_id(playlist)
       list.tracks.each do |track|
-        tracks << { id: track.id, name: track.name }
+        tracks << track
       end
     end
     return tracks
